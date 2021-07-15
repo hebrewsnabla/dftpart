84505a6cbc12fcf9ec437c0a34b4a6e722d51aaa
hebrewsnabla <srwang20@fudan.edu.cn>
2021-07-15 22:05:33 +0800

    clean

--- mp_example/32ane/32ane.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-export GAUSS_SCRDIR=/tmp/liwei01/gaussian/liaokang/
-mkdir -p /tmp/liwei01/gaussian/liaokang/69924092/
-make -s -j4 -f 32ane.make
-rm -rf /scratch/liwei01/gaussian/69924092
--- mp_example/32ane/eda_ex.py
+++
@@ -1,7 +0,0 @@
-from dftpart import scfeda
-
-fr0tot = scfeda.EDA()
-fr0tot.method = ['hf','6-31gs']
-fr0tot.output = '32ane_tot'
-fr0tot.gjf = '32ane.gjf'
-fr0tot.kernel()
